937bf8fe33a1b9f86f9a0b75f00d5a2d6f8c428b
Stefan <smmuijs2002@gmail.com>
2026-09-10 10:42:57 +0200

    Start Collision, Added PlayerFloor

---
+++ Assets/Scripts/Player/PlayerFloor.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerFloor : MonoBehaviour
+{
+    public GameObject collisionBottom;
+    public GameObject collisionTop;
+
+    public int playerHeight = 0;
+
+    void Start()
+    {
+        SetFloor(1);
+    }
+
+    public void SetFloor(int floor)
+    {
+        playerHeight = floor;
+
+        if (floor == 0)
+        {
+            collisionBottom.SetActive(true);
+            collisionTop.SetActive(false);
+        }
+        else if (floor == 1)
+        {
+            collisionBottom.SetActive(false);
+            collisionTop.SetActive(true);
+        }
+    }
+}
