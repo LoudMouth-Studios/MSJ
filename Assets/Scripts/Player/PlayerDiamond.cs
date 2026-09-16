bb1ef8068b58ec3fe3ad2937d326ed0e08c0f5a4
marcelboo <1038695@hr.nl>
2026-09-16 18:45:44 +0200

    fixed the compiler error

--- Assets/Scripts/Player/PlayerDiamond.cs
+++ Assets/Scripts/Player/PlayerDiamond.cs
@@ -9,7 +9,7 @@ public class Diamond : MonoBehaviour
 
     private void Start()
     {
-        interactButton.interactable ?= false;
+        interactButton.interactable = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -35,6 +35,6 @@ public class Diamond : MonoBehaviour
         if (!playerInRange)
             return;
 
-        GameManager.Instance?.FinishLevel();
+        GameManager.Instance.FinishLevel();
     }
 }
