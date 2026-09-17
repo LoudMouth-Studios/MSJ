e65bf46c1579507a2437686dbc5b9114e7d6279d
marcelboo <101633688+marcelboo@users.noreply.github.com>
2026-09-17 14:52:37 +0200

    Deleted old collision. Made new collision.

--- Assets/Scripts/Player/PlayerDiamond.cs
+++ Assets/Scripts/Player/PlayerDiamond.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Diamond : MonoBehaviour, IInteractable
 {
-    bool collected;
-
     public void Interact()
     {
-        if (collected) return;
-        collected = true;
+        // if (collected) return;
+        // collected = true;
 
         // TODO: show a "carrying diamond" icon on the Player here.
         gameObject.SetActive(false); // removes the diamond tile from the level
     }
+
+    bool collected;
 }
