0b9b49438d831cc11cc4ed1b2e9ae76882381858
marcelboo <101633688+marcelboo@users.noreply.github.com>
2026-09-25 11:46:57 +0200

    Update GameManager.cs

---
+++ Assets/Scripts/Shared/GameManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    string currentLevelName;
+    public bool HasDiamond { get; private set; }
+    public int LastLevelStars { get; private set; }
+    public static bool IsPaused { get; set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += (scene, mode) => HasDiamond = false;
+    }
+    public void SetLevelStars(int stars)
+    {
+        LastLevelStars = stars;
+    }
+    public void CollectDiamond()
+    {
+        HasDiamond = true;
+    }
+
+    public void StartScreen()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void FinishLevel()
+    {
+        SceneManager.LoadScene("Diamond stolen");
+    }
+    
+    public void TriggerDefeat()
+    {
+        currentLevelName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene("Defeat");
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(currentLevelName);
+    }
+}
